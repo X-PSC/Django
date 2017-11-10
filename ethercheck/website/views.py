d3905b4f23fc7e8529a38f7a0db2b52f9ea7a84a
Antoine Carossio <antoine.carossio@me.com>
2017-11-10 15:00:02 +0100

    Affichage des transactions avec DataTable

--- ethercheck/website/views.py
+++ ethercheck/website/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from website.models import Transaction
 
 # Create your views here.
 
 def home(request):
-	return render(request, 'index.html')
+	transactions = Transaction.objects.all()
+	return render(request, 'index.html', locals())
 
