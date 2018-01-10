eee0759bebf236bee25acc426282241daf868861
charlo <marc.alefsen@protonmail.com>
2018-01-10 15:51:18 +0100

    affichage des variations de cours

--- ethercheck/website/views.py
+++ ethercheck/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response
 from website.models import Transaction
 from website.models import Alias
+from website.models import Block
 import requests, json
 
 # Create your views here.
@@ -8,10 +9,13 @@ import requests, json
 def home(request):
 	return render(request, 'index.html', locals())
 
-
 def transactions(request):
 	transactions = Transaction.objects.all()
 	return render(request, 'transactions.html', locals())
+
+def blocks(request):
+	blocks = Block.objects.all()
+	return render(request, 'blocks.html', locals())
 
 def alias(request):
 	alias = Alias.objects.all()
@@ -25,9 +29,9 @@ def graphData(request):
 
 def tauxChange(request):
 	changeInformation=json.loads(requests.get('https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=EUR').text)
-	usd=changeInformation[0]["price_usd"]
-	eur=changeInformation[0]["price_eur"]
-	bit=changeInformation[0]["price_btc"]
+	usd=round(float(changeInformation[0]["price_usd"]),2)
+	eur=round(float(changeInformation[0]["price_eur"]),2)
+	bit=round(float(changeInformation[0]["price_btc"]),8)
 	evo_1h=changeInformation[0]["percent_change_1h"]
 	evo_24h=changeInformation[0]["percent_change_24h"]
 	evo_7d=changeInformation[0]["percent_change_7d"]
