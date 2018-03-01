db356d0cc459fcc70b3097da0fd73659808d9af0
lucasdfr <lucas_delcros@hotmail.com>
2018-03-01 13:49:58 +0100

    Tests on visualisations

--- ethercheck/website/static/js/vis.js
+++ ethercheck/website/static/js/vis.js
@@ -1,73 +1,75 @@
 
+
 var svg = d3.select("svg"),
-width = +svg.attr("width"),
-height = +svg.attr("height");
+    width = window.innerHeight,
+    height = window.innerHeight,
+    color = d3.scaleOrdinal(d3.schemeCategory10);
 
-var color = d3.scaleOrdinal(d3.schemeCategory20);
+var a = {id: "a"},
+    b = {id: "b"},
+    c = {id: "c"},
+    nodes = [a, b, c],
+    links = [];
 
-var simulation = d3.forceSimulation()
-.force("link", d3.forceLink().id(function(d) { return d.id; }))
-.force("charge", d3.forceManyBody())
-.force("center", d3.forceCenter(width / 2, height / 2));
+var simulation = d3.forceSimulation(nodes)
+    .force("charge", d3.forceManyBody().strength(-20))
+    .force("link", d3.forceLink(links).distance(200))
+    .force("x", d3.forceX())
+    .force("y", d3.forceY())
+    .alphaTarget(1)
+    .on("tick", ticked);
 
-d3.json("static/js/miserables.json", function(error, graph) {
-if (error) throw error;
+var g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
+    link = g.append("g").attr("stroke", "#000").attr("stroke-width", 1.5).selectAll(".link"),
+    node = g.append("g").attr("stroke", "#fff").attr("stroke-width", 1.5).selectAll(".node");
 
-var link = svg.append("g")
-  .attr("class", "links")
-.selectAll("line")
-.data(graph.links)
-.enter().append("line")
-  .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+restart();
 
-var node = svg.append("g")
-  .attr("class", "nodes")
-.selectAll("circle")
-.data(graph.nodes)
-.enter().append("circle")
-  .attr("r", 5)
-  .attr("fill", function(d) { return color(d.group); })
-  .call(d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended));
+d3.timeout(function() {
+  links.push({source: a, target: b}); // Add a-b.
+  links.push({source: b, target: c}); // Add b-c.
+  links.push({source: c, target: a}); // Add c-a.
+  restart();
+}, 1000);
 
-node.append("title")
-  .text(function(d) { return d.id; });
+d3.interval(function() {
+  nodes.pop(); // Remove c.
+  links.pop(); // Remove c-a.
+  links.pop(); // Remove b-c.
+  restart();
+}, 2000, d3.now());
 
-simulation
-  .nodes(graph.nodes)
-  .on("tick", ticked);
+d3.interval(function() {
+  nodes.push(c); // Re-add c.
+  links.push({source: b, target: c}); // Re-add b-c.
+  links.push({source: c, target: a}); // Re-add c-a.
+  restart();
+}, 2000, d3.now() + 1000);
 
-simulation.force("link")
-  .links(graph.links);
+function restart() {
+
+  // Apply the general update pattern to the nodes.
+  node = node.data(nodes, function(d) { return d.id;});
+  node.exit().remove();
+  node = node.enter().append("circle").attr("fill", function(d) { return color(d.id); }).attr("r", 8).merge(node);
+
+  // Apply the general update pattern to the links.
+  link = link.data(links, function(d) { return d.source.id + "-" + d.target.id; });
+  link.exit().remove();
+  link = link.enter().append("line").merge(link);
+
+  // Update and restart the simulation.
+  simulation.nodes(nodes);
+  simulation.force("link").links(links);
+  simulation.alpha(1).restart();
+}
 
 function ticked() {
-link
-    .attr("x1", function(d) { return d.source.x; })
-    .attr("y1", function(d) { return d.source.y; })
-    .attr("x2", function(d) { return d.target.x; })
-    .attr("y2", function(d) { return d.target.y; });
+  node.attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
 
-node
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; });
-}
-});
-
-function dragstarted(d) {
-if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-d.fx = d.x;
-d.fy = d.y;
-}
-
-function dragged(d) {
-d.fx = d3.event.x;
-d.fy = d3.event.y;
-}
-
-function dragended(d) {
-if (!d3.event.active) simulation.alphaTarget(0);
-d.fx = null;
-d.fy = null;
+  link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
 }
